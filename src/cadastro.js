fbc22bd97234eee7f21e8f47992f5bd9a7a35a80
Sergio <sergioricjr7@gmail.com>
2023-04-18 17:40:57 -0300

    sim

--- src/cadastro.js
+++ src/cadastro.js
@@ -1,9 +1,14 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export const Cadastro = () => {
     const [usuario, setUsuario] = useState('')
     const [usuarios, setUsuarios] = useState(["sergio", "josé"])
-    
+    const [conta, setConta] = useState(2)
+
+    useEffect(()=>{
+        setConta(usuarios.length)
+    }, [usuarios])
+
     function adicionaUsuario() {
         setUsuarios([...usuarios, usuario])
         setUsuario("")
@@ -11,6 +16,7 @@ export const Cadastro = () => {
 
     return (
         <div>
+            <h1>Total: {conta}</h1>
             <input onChange={(event)=> setUsuario(event.target.value)} value={usuario}></input>
             <input type="button" 
                 onClick={adicionaUsuario}
